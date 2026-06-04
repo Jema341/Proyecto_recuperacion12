c226545ae6bd83fa1a7d353317930df777acb670
Abigail Toapanta <karen.toapanta4003@utc.edu.ec>
2026-06-04 15:09:33 -0500

    Configurar ALLOWED_HOSTS desde variables de entorno

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-kp3=!y#gmp&o5hpua-4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [host.strip() for host in os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',') if host.strip()]
 
 
 # Application definition
