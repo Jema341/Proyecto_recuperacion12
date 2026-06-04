99ca0a6fea8d9dc9442e339fb5960bca72db1468
Your Name <you@example.com>
2026-06-04 16:18:28 -0500

    inicio desde login

--- inicio/apps.py
+++ inicio/apps.py
@@ -5,3 +5,7 @@ class InicioConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'inicio'
     verbose_name = 'Sistema de Inicio'
+    
+    def ready(self):
+        import inicio.signals
+        import inicio.models
