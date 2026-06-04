6830ce2a4cfb40fde575239f02dba05960aabde6
Nayeli Vega <veganayeli925@gmail.com>
2026-06-04 00:40:39 -0500

    actualizacion

--- inicio/urls.py
+++ inicio/urls.py
@@ -4,6 +4,13 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='inicio'),
+    path('', views.dashboard, name='dashboard'),
+    path('ui-icons/', views.ui_icons, name='ui_icons'),
+    path('forms/', views.forms, name='forms'),
+    path('tables/', views.tables, name='tables'),
+    path('calendar/', views.calendar, name='calendar'),
+    path('profile/', views.profile, name='profile'),
+    path('login/', views.login_view, name='login'),
+    path('registration/', views.registration, name='registration'),
     
 ]
--- inicio/views.py
+++ inicio/views.py
@@ -8,3 +8,24 @@ def inicio(request):
 
 def dashboard(request):
   return render(request, 'dashboard.html')
+
+def ui_icons(request):
+  return render(request, 'ui-icons.html')
+
+def forms(request):
+  return render(request, 'forms.html')
+
+def tables(request):
+  return render(request, 'tables.html')
+
+def calendar(request):
+  return render(request, 'calendar.html')
+
+def profile(request):
+  return render(request, 'profile.html')
+
+def login_view(request):
+  return render(request, 'login.html')
+
+def registration(request):
+  return render(request, 'registration.html')
